cf10107c27c74f22097d073450ffdb781dbadfec
Mike Lapshin <mikhail.a.lapshin@gmail.com>
2013-08-11 21:06:48 +0400

    handle SIGINT nicely

--- lib/vagrant-foodtaster-server/server_command.rb
+++ lib/vagrant-foodtaster-server/server_command.rb
@@ -1,4 +1,4 @@
-require 'drb/drb'
+require 'drb'
 require_relative 'server'
 
 class VagrantFoodtasterServer
@@ -6,11 +6,17 @@ class VagrantFoodtasterServer
     def execute
       ui = @env.ui
       argv = parse_options
-      port_number = argv.size == 0 ? 8787 : argv[0].to_i
 
-      ui.info "Starting Foodtaster Server at druby://localhost:#{port_number}"
+      port_number = argv.size == 0 ? 8787 : argv[0].to_i
       DRb.start_service "druby://localhost:#{port_number}", VagrantFoodtasterServer::Server.new(@app, @env)
-      DRb.thread.join
+      ui.info "Started Foodtaster Server at druby://localhost:#{port_number}"
+      ui.info "Press Ctrl+C to stop."
+
+      begin
+        DRb.thread.join
+      rescue Interrupt
+        DRb.stop_service
+      end
     end
   end
 end
